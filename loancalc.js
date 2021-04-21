68261afd65dd53439f97d2c40ac806aeee14ddd4
Nasreen Khalid <nasreen.1@live.com>
2021-04-21 15:55:20 +0300

    loan calc

--- loancalc.js
+++ loancalc.js
@@ -5,6 +5,10 @@ const loanAmt = document.getElementById('loan-amount');
 const calcBtn = document.querySelector(".btn-submit")
 const intRate = document.getElementById("interestRate")
 const loanMonths = document.getElementById("loan-months")
+const monthlyRepayAmount = document.getElementById("monthly-repay-amount")
+const monthlyIncome = document.getElementById("month-income")
+const loanResults = document.getElementById('loan-results')
+
 var monthlyPayment;
 
 // The following function limits the loan amt to decimal places
@@ -35,25 +39,36 @@ function decimalCheck(){
 calcBtn.addEventListener("click", function(e){
 e.preventDefault();
 var loanValue= loanAmt.value;
-console.log(loanMonths.value)
 var interestRateMonthly = (((intRate.value)/100)/12).toFixed(4);
-console.log(interestRateMonthly)
-// monthlyPayment = (loanValue * (intRate*(Math.pow(1 + intRate)),loanMonths))/(Math.pow(1+intRate),loanMonths)-1
-// console.log(monthlyPayment.toFixed(2))
+// converting the interest rate to number and then increment by 1
 let s = 1 + Number(interestRateMonthly);
 var x = loanValue * interestRateMonthly * (Math.pow((s),loanMonths.value));
-console.log(Math.pow((s),loanMonths.value))
-console.log(s)
-console.log((x))
 var y = ((Math.pow((s),loanMonths.value))-1);
-console.log(y)
+var resultMonthlyPayments = x / y;
+console.log(resultMonthlyPayments)
 
-console.log((x)/(y))
+displayResults(resultMonthlyPayments);
+
 })
 
-//let log10Y = Math.log(1+interestRateMonthly)*loanMonths.value/Math.log(10)
+function displayResults(result){
 
-  
-function getBaseLog(x, y) {
-  return Math.log(y) / Math.log(x);
+monthlyRepayAmount.innerHTML = `$ ${result.toFixed(3)}`
+
+var halfMonthlyIncome = monthlyIncome.value * 0.5;
+console.log(monthlyIncome.value)
+console.log(halfMonthlyIncome)
+
+if(result > halfMonthlyIncome){
+loanResults.classList.add('red')
+loanResults.innerHTML= `Caution: <br>We noticed that your monthly repayment is greater than 50% of your
+                        net monthly income (Given that you're not servicing any other loans or debt).
+                        We would like to suggest, that you go for a loan with lower amount.`
+
+} else if(result < halfMonthlyIncome){
+  loanResults.classList.add('green');
+  loanResults.innerHTML = `Good to go! <br> We noticed that your monthly repayment is less than 50% of your
+                            net monthly income (Given that you're not servicing any other loans or debt).
+                            We believe that repaying the loan won't be difficult for you.`
+}
 }
